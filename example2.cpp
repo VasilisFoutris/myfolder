bca1b421d80bce6f6316b571f147dfdef23e2f5e
sdi2000231 <sdi2000231@uoa.gr>
2021-12-08 23:25:03 +0200

    8/12

--- example2.cpp
+++ example2.cpp
@@ -1,40 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <string>
-//#include <cstdlib>
-//#include <ctime>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+
 using namespace std;
 
 
 
 class base { 
-    
+// Το αντικείμενο βάση έχει μόνο δομικό ρόλο στην βιβλιοθήκη
 public:
     
     base(){
-
-		cout << " Base under construction\n " << endl;
+       
+		cout << "Base under construction\n " << endl;
+        
     }
 
 	~base(){
 
-		cout << " Base destruction phase\n " << endl;
+		cout << "Base destruction phase\n " << endl;
+
 	}
 };
 
-class closet_shelf {
+class upper_closet_shelf {
   
 public:
 
-    closet_shelf(){
+    upper_closet_shelf(){
 
-		cout << " Closet shelfs under construction\n " << endl;
+		cout << "Closet shelf under construction\n " << endl;
 
 	}
 	
-	~closet_shelf(){
+	~upper_closet_shelf(){
 
-		cout << " Closet shelfs destruction phase\n " << endl;
+		cout << "Closet shelf destruction phase\n " << endl;
+
+	}
+
+};
+
+class bottom_closet_shelf {
+  
+public:
+
+    bottom_closet_shelf(){
+
+		cout << "Bottom shelf under construction\n " << endl;
+
+	}
+	
+	~bottom_closet_shelf(){
+
+		cout << "Bottom shelf destruction phase\n " << endl;
 
 	}
 
@@ -46,31 +68,67 @@ public:
 
     closet(){
 		
-		cout << " Closet under construction\n " << endl;
+		cout << "Closet under construction\n " << endl;
 
 	}
 
 	~closet(){
 
-		cout << " Closet destruction phase\n " << endl;
+		cout << "Closet destruction phase\n " << endl;
 
 	}
     
 };
 
-class shelf {
+class upper_shelf {
 
 public:
 
-    shelf(){
+    upper_shelf(){
 
-		cout << " Shelfs under construction\n " << endl;
+		cout << "Upper shelf under construction\n " << endl;
 
 	}
 
-	~shelf(){
+	~upper_shelf(){
 
-		cout << " Shelfs destruction phase\n " << endl;
+		cout << "Upper shelf destruction phase\n " << endl;
+
+	}
+
+};
+
+class mid_shelf {
+
+public:
+
+    mid_shelf(){
+
+		cout << "Mid shelf under construction\n " << endl;
+
+	}
+
+	~mid_shelf(){
+
+		cout << "Mid shelf destruction phase\n " << endl;
+
+	}
+
+};
+
+class bottom_shelf {
+
+public:
+
+    bottom_shelf(){
+
+		cout << "Bottom shelf under construction\n " << endl;
+
+	}
+
+	~bottom_shelf(){
+
+		cout << "Bottom shelf destruction phase\n " << endl;
 
 	}
 
@@ -78,166 +136,253 @@ public:
 
 class book{
 	//Στην κλάση βιβλίο δημιουργούνται τα βιβλία 
-    int L;
-	char book_title[20];
-	int isbn;
-    char writer[20];
-    
+
 public:
+    string  gen_random_aplha( int len1 = 10 ) {
+    
+    std::string s1;
+    static const char alpha[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789";
 
-	book(){
-
-
-        isbn = 1000;
-		L = 2;   
-        cout << "Enter Book Title " << endl ;
-		cin >> book_title;
-                
-        cout << "Enter Writer : " << endl ;
-        cin >> writer;        
-        cout << " \n\n " << endl ;
-                
-        isbn++;
-        //cout << isbn << endl ;
-
-        cout << "Book Title : " << book_title << endl ;
-        cout << "Book Number : " << L << endl ;
-        cout << "ISBN : " << isbn << endl ;
-        cout << "Writer : " << writer << "\n\n" << endl ;  
-        
-        int books = L;
-
+    for ( int i = 0; i < len1; ++i ) {
+        s1 += alpha[ rand() % ( sizeof( alpha ) - 1 ) ];
+    }
+    return s1;
     }
 
+    string gen_random_num ( int len2 = 10 ){
+    
+    std::string s2;
+    static const char num[] =
+        "0123456789";
+
+       for ( int d = 0; d < len2; ++d ) {
+        s2 += num[ rand() % ( sizeof( num ) - 1)];
+    }
+    return s2;
+    }
+    
+    string  gen_random_beta( int len3 = 10 ) {
+    string s3;
+    static const char alpha[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int r = 0; r < len3; ++r) {
+        s3 += alpha[ rand() % ( sizeof( alpha ) - 1) ];
+    }
+    return s3;
+    }
+
+	book(){
+      
+        cout << "Book Title : " << gen_random_aplha() << endl;
+
+        cout << "ISBN : " << gen_random_num() << endl;
+
+        cout << "Writer : " << gen_random_beta() << "\n\n" << endl;  
+       
+    }
 
 	~book(){
-
-		cout << " Books destroyed " << endl;
-	}
+        cout << "-------------------------" << endl;
+		cout << "Book destroyed" << endl;
+        cout << "-------------------------\n" << endl;
+	}   
 };
 
 int place_book(){
 
-    return 0;
+    int t = 0;
+
+    //srand ( time ( NULL ) );
+    t = rand() % 5 + 1;
+    
+    switch ( t ){
+////   
+    case 1:
+////Αν t = 1 τοποθετεί το βιβλίο στο πρώτο ράφι
+    
+    int shelf1[10];
+
+    cout << "-------------------------" << endl;
+    cout << "Book placed in position 1 - Upper Shelf" << endl;
+    cout << "-------------------------\n" << endl;
+    
+    break;
+
+////
+    case 2:
+////Αν t = 2 τοποθετεί το βιβλίο στο δεύτερο ράφι
+    
+    int shelf2[10];
+
+    cout << "-------------------------" << endl;
+    cout << "Book placed in position 2 - Mid Shelf" << endl;
+    cout << "-------------------------\n" << endl;
+    
+    break;
+
+////
+    case 3:
+////Αν t = 3 τοποθετεί το βιβλίο στο τρίτο ράφι
+    
+    int shelf3[10];
+
+    cout << "-------------------------" << endl;    
+    cout << "Book placed in position 3 - Lower Shelf" << endl;
+    cout << "-------------------------\n" << endl;
+    
+    break;
+
+////
+    case 4:
+////Αν t = 4 τοποθετεί το βιβλίο στο πρώτο ράφι του ντουλαπιού   
+    
+    int closet1[10];
+
+    cout << "-------------------------" << endl;
+    cout << "Book placed in position 4 - Upper Closet Shelf" << endl;
+    cout << "-------------------------\n" << endl;
+    
+    break;
+
+////
+    case 5:
+////Αν t = 5 τοποθετεί το βιβλίο στο δεύτερο ράφι του ντουλαπιού
+    
+    int closet2[10];
+
+    cout << "-------------------------" << endl;
+    cout << "Book placed in position 5 - Bottom Closet Shelf" << endl;
+    cout << "-------------------------\n" << endl;
+    
+    break;
+////
+    }
+
+    return 0; 
 }
 
 int take_book(){
 
+    int t;
+    t = rand() % 5 + 1;
+
+    switch ( t ){
+////
+        case 1:
+////Αν t = 1 αφαιρεί το βιβλίο απο το πρώτο ράφι      
+        cout << "-------------------------" << endl;
+        cout << "Book has been revomed from position 1 - Upper Shelf" << endl;
+        cout << "-------------------------\n" << endl;
+
+        break;
+
+////
+        case 2:
+////Αν t = 2 αφαιρεί το βιβλίο απο το δεύτερο ράφι      
+        cout << "-------------------------" << endl;
+        cout << "Book has been revomed from position 2 - Mid Shelf" << endl;
+        cout << "-------------------------\n" << endl;
+
+        break;
+
+////
+        case 3:
+////Αν t = 3 αφαιρεί το βιβλίο απο το τρίτο ράφι      
+        cout << "-------------------------" << endl;
+        cout << "Book has been revomed from position 3 - Bottom Shelf" << endl;
+        cout << "-------------------------\n" << endl;
+
+        break;
+
+////
+        case 4:
+////Αν t = 4 αφαιρεί το βιβλίο από το πρώτο ράφι του ντουλαπιού
+        cout << "-------------------------" << endl;
+        cout << "Book has been revomed from position 4 - Upper Closet Shelf" << endl;
+        cout << "-------------------------\n" << endl;
+
+        break;
+
+////
+        case 5:
+////Αν t = 5 αφαιρεί το βιβλίο από το δεύτερο ράφι του ντουλαπιού
+        cout << "-------------------------" << endl;
+        cout << "Book has been revomed from position 5 - Bottom Closet Shelf" << endl;
+        cout << "-------------------------\n" << endl;
+
+        break;
+////
+    }
+
     return 0;
 }       
 
-//int book_generator(){
-    // Εδώ δημιουργείται τυχαία ο αριθμός βιβλίων που θα προστεθούν στη βιβλιοθήκη 
-	//srand( ( int ) time ( 0 ) );
-	//int i = 0;
-    //int L;
-	
-	//L = ( rand () % 100) + 20;
-	//cout << L << endl;
-
-	//return L;
-//}
-
-//int book_ISBN_generator(){
-    // Εδώ δημιουργείται o τυχαίος κλειδάριθμος ISBN των βιβλίων 
-    //srand( ( int ) time ( 0 ) );
-	//int i = 0; 
-    //int k;
-    //int isbn;
-    //k = book_generator();
-
-    //for( k > 10; k < 30; k++ ) {
-	    //isbn = ( rand() % 10 ) + 1 ;
-        //cout << isbn << endl; 
-        //return isbn;
-	//}
-    //return isbn;
-//}
-// *************************************************************************************** ΒΓΑΖΕΙ ΣΑΧΛΑΜΑΡΕΣ ΤΥΧΑΙΑ ISBN ΕΔΩ ΦΤΙΑΞΤΟ
-//int ISBN_string(){
-   
-	//int isbn_str_length = 10;
-    //int k = book_generator();
-
-    //for( int g = 0; g <= k;  g++ ){
-        //int x = book_ISBN_generator();
-        //string s1( isbn_str_length , x );
-	    //cout << " ISBN: "<< s1 << endl;
-        
-   //}
-//}
-
 // Ο constructor δημιουργεί την βιβλιοθήκη σαν σύνολο ραφιών και ντουλαπιών ενώ καλεί τις κλάσεις να δημιουργήσουν τα αντικείμενα
-int construct(){
+// O destructor καταστρέφει τα αντικείμενα όταν το πρόγραμμα τελειώσει  
+int construct_destruct(){
 
-    cout << " BOOK CREATION SECTOR :\n\n "<< endl;
+    srand ( time ( NULL ) );
+    
+    cout << "\nBOOK CREATION SECTOR :\n\n "<< endl;
     //εδώ η διαδικασία δημιουργίας των βιβλίων
-    book obj1;
 
-    cout << " Starting library construction\n " << endl; 
+    // δημιουργούμε όσα αντικείμενα book θέλουμε 
+    book obj01;
+    book obj02;
+    //book obj03;
 
-    cout << " Base under construction\n " << endl;
+    cout << "Books have been configured "  << endl; 
+    cout << "-------------------------\n" << endl;
+    cout << "Starting library construction\n " << endl; 
+
+    //cout << " Base under construction\n " << endl;
     //εδώ η κλήση της κλάσης base για δημιουργ΄΄ια
+    base obj1;
 
-    cout << " Closet shelfs under construction\n " << endl;
-    //εδώ η κλήση της κλάσης closet_shelf για δημιουργ΄΄ια
+    //εδώ η κλήση της κλάσης upper_closet_shelf για δημιουργ΄΄ια
+    upper_closet_shelf obj2;
 
-    cout << " Closet under construction\n " << endl;
+    //εδώ η κλήση της κλάσης bottom_closet_shelf για δημιουργ΄΄ια
+    bottom_closet_shelf obj3;
+
     //εδώ η κλήση της κλάσης closet για δημιουργ΄΄ια
+    closet obj4;
 
-    cout << " Shelfs under construction\n " << endl;
-    //εδώ η κλήση της κλάσης shelf για δημιουργ΄΄ια
+    //εδώ η κλήση της κλάσης upper_shelf για δημιουργ΄΄ια
+    upper_shelf obj5;
+
+    //εδώ η κλήση της κλάσης mid_shelf για δημιουργ΄΄ια
+    mid_shelf obj6;
+
+    //εδώ η κλήση της κλάσης bottom_shelf για δημιουργ΄΄ια
+    bottom_shelf obj7;
 
     cout << " Library finished construction\n " << endl;
     // Η βιβλιοθήκη κατασκευάστηκε
     
-    return true;
-}
-
-//Ο destructor καταστρέφει την βιβλιοθήκη σαν σύνολο
-int destruct(){
-
-
-
-    cout << " Starting destruction of the library\n " << endl;
-    //εδώ η διαδικασία καταστοφής της βιβλιοθήκης
-
-    cout << " Shelfs destruction phase\n " << endl;
-    //εδώ η διαδικασία καταστοφής των ραφιών
-
-    cout << " Closet shelfs destruction phase\n " << endl;
-    //εδώ η διαδικασία καταστοφής των ραφιών στο ντουλ΄σπι
-
-    cout << " Closet destruction phase\n " << endl;
-    //εδώ η διαδικασία καταστοφής του ντουλαπιο΄υ
-
-    cout << " Base destruction phase\n " << endl;
-    //εδώ η διαδικασία καταστοφής της βάσης
-
-    cout << " Library completely destroyed\n " << endl;
-    // Η βιβλιοθήκη καταστράφηκε
+    //Εδώ ξεκινάει η διαδικασία τοποθέτησης και αφαίρεσης των βιβλίων από τη βιβλιοθήκη
+    int books = 2;
     
+    for ( int m = 0; m < books; m++ ){
+        place_book();
+    }
+    
+    for ( int n = 0; n < books; n++ ){
+        take_book();
+    }
+
     return true;
 }
 
 int main(){
-
-    int q = 1;
-        
-    construct();
-
-	base obj2;
     
-    closet_shelf obj3;
 
-    closet obj4;
+    construct_destruct();
 
-    shelf obj5;
-
-    q++;
-
-    if ( q == 2 ){
-        destruct();
-    }
     return 0;
 }
