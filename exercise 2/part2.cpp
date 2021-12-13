911ea9a4336fa72e338cb28eb698d3a41246e14e
sdi2000231 <sdi2000231@uoa.gr>
2021-12-14 01:06:18 +0200

    14/12

--- exercise 2/part2.cpp
+++ exercise 2/part2.cpp
@@ -11,71 +11,218 @@ class cashier{
 
     public:
     
-    cashier(){
+        int customers_served;
+    
+        cashier( int customers );
 
-        cout << "Cashiers opening\n" << endl;
+        int is_open();
 
-    }
+        void open();
 
-    ~cashier(){
+        void close();
 
-        cout << "Cashiers closed for today\n" << endl;
+        void serve();
+    
+        int is_free();
 
-    }
+        void free();
 
+        void open_flag();
+        
+        void serving_flag();
+
+        ~cashier();
+
+
+    private:
+
+        int cust;
+        int next;
+        int *cashiers_serving;
+        cashier *total_cashier;
+      
 };
 
+cashier::cashier( int customers ){
+    
+    cust = customers;
+    customers_served = 0;
+    
+    open();
+    open_flag();
+    serve();
+    serving_flag();
+    free();
+    customers_served++;
+    cout << "Customers served:"<< customers_served << endl;
+
+    close();
+}
+
+cashier::~cashier(){
+    
+    cout << "Cashier has been destroyed\n" << endl;
+
+}
+
+
+
+void cashier::open(){
+
+    cout << "Cashier is open!\n" << endl;
+
+}
+
+int cashier::is_open(){
+
+
+  return 1;
+}
+
+void cashier::close(){
+
+    cout << "Cashier is closed!\n" << endl;
+
+}
+
+void cashier::serve(){
+
+    cout << "Cashier is serving!\n" << endl;
+
+}
+
+void cashier::free(){
+
+    cout << "Cashier is free!\n" << endl;
+
+}
+
+int cashier::is_free(){
+
+
+
+    return 1;
+}
+
+void cashier::open_flag(){
+
+    if( is_open ){
+
+        cout << "Cashier open\n" << endl;
+        open();
+        
+    }
+}
+
+void cashier::serving_flag(){
+
+    if( is_free ){
+
+        free();
+
+    }
+    else{
+
+        cout << "Cashier currently serving another customer\n" << endl;
+        
+    }
+
+}
 
 class bank{
 
     public:
     
-    bank(){
-
-        cout << "\nBank cosntruction phase\n" << endl;
-
-    }
-
-    ~bank(){
+        bank( int cust );
         
-        cout << "Bank destruction phase\n" << endl;
+        int curr_serving( int customers );
 
-    }
+        int chack_to_open();
+
+        int check_to_close();
+
+        int waiting_customers();
+
+        int enter();
+
+        int exit();
+
+        ~bank();
+
+    private:
+
+        cashier cashier1;
+        cashier cashier2;
+        cashier cashier3;
+        cashier cashier4;
+        cashier cashier5;
 
 };
 
-int curr_serving(){
 
-}
-
-int enter(){
-
-}
-
-int serve(){
+bank::bank( int customers ) : cashier1 ( customers ), cashier2 ( customers ), cashier3( customers ), cashier4( customers ), cashier5( customers ) {
     
+    int last_customer = 1;
+    int first_paper = 1; 
+
+    cout << "\nCustomers:" << endl;
+    cin >> last_customer ;
+
+    cout << "\n" << endl;
+    cout << "-------------------------" << endl;
+    cout << "First paper is : " << first_paper << endl;
+    cout << "\n" << endl;
+
+    for( int curr_serving = 1; curr_serving <= last_customer; curr_serving++ ){
+        
+        cout << "Currently serving: " << curr_serving << endl;
+        cout << "\n" << endl;
+
+    }
+
 }
 
-int construct_destruct(){
+int bank::curr_serving( int customers ){
 
-    bank obj1;
+    return 0;
+}
 
-    cashier obj2;
+int bank::chack_to_open(){
 
-    cashier obj3;
+    return 0;
+}
 
-    cashier obj4;
+int bank::check_to_close(){
 
-    cashier obj5;
+    return 0;
+}
 
-    cashier obj6;
-    
-    return true;
+int bank::waiting_customers(){
+
+    return 0;
+}
+
+int bank::enter(){
+
+    return 0;
+}
+
+int bank::exit(){
+
+    return 0;
+}
+
+bank::~bank (){
+
+    cout << "Bank destruction phase\n" << endl;
+
 }
 
 int main(){
     
-    construct_destruct();
+    int n;
+
+    bank b1(10);
 
     return 0;
 }
